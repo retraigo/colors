d81f6bb77dedc6c7b150b1462d1c13a1062b480c
NeTT <nett@nett.moe>
2024-03-03 15:59:10 +0530

    add module doc

--- mod.ts
+++ mod.ts
@@ -1,1 +1,6 @@
-export * from "./src/mod.ts"
+/**
+ * A collection of functions to extract information from colors,
+ * generate shades and tints, convert to other color spaces, etc.
+ * @module
+ */
+export * from "./src/mod.ts";
